f1189d17c1c6e74d0d87338baf73e55dcc25f2cc
Alex Zero <me@alex0.net>
2022-12-23 20:20:33 +0000

    Automated release commit for 2.11.0

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 ## Ascent: Override II
 
-This is a proprietary modpack used by the Minecraft server of the same name.
+This is the modpack used by the Minecraft server of the same name.
+
+### Structure
+
+- `config`: Minecraft configuration files, which end up in the `config` folder.
+- `data`: Symlink to `config/openloader/data/default/data`, which is our default data pack.
+- `kubejs`: KubeJS recipe and gameplay tweaks.
+- `mods`: Fabric/Quilt mods, which end up in the `mods` folder.
+- `resources`: Symlink to `config/openloader/resources`, which contains our resource packs.
 
 ### Development
 
--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,15 +1,17 @@
 // priority: 0
 
 ServerEvents.recipes(event => {
-	event.remove({ id: 'tempad:tempad' })
-	event.remove({ id: 'tempad:he_who_remains_tempad' })
+	event.remove({ id: 'tempad:tempad' });
+	event.remove({ id: 'tempad:he_who_remains_tempad' });
 
 	// remove blast furnace recipe for ad astra steel
 	event.remove({
 		type: 'minecraft:blasting',
 		output: 'ad_astra:steel_ingot'
-	})
-})
+	});
+
+	// replace ad astra book recipe with 
+});
 
 ServerEvents.tags('item', event => {
 	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
@@ -17,4 +19,4 @@ ServerEvents.tags('item', event => {
 
 	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
 	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
-})
+});
